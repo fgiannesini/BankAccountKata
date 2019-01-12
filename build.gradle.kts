ae5edc1b6f4e61aa0772108f478a864622d513e0
fabien.giannesini <fabien.giannesini@gmail.com>
2019-01-12 17:35:16 +0100

    Set versions for gradle script

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,10 +7,15 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.3.+")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.3.2")
 }
 
 
 tasks.getting(Test::class) {
     useJUnitPlatform()
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
